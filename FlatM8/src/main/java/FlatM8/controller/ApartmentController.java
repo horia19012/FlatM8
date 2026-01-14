ff7f8b59b3c3cb8789359ab52c54cd87a4ede5cc
horia19012 <apostol_dan1901@yahoo.com>
2026-01-14 21:59:30 +0200

    apartment api

--- FlatM8/src/main/java/FlatM8/controller/ApartmentController.java
+++ FlatM8/src/main/java/FlatM8/controller/ApartmentController.java
@@ -1,8 +1,31 @@
 package FlatM8.controller;
 
+import FlatM8.model.Apartment;
+import FlatM8.service.ApartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/apartments")
 public class ApartmentController {
 
+    @Autowired
+    private ApartmentService apartmentService;
+
+    @GetMapping
+    public List<Apartment> getAllApartments() {
+        return this.apartmentService.getAllApartments();
+    }
+
+    @GetMapping
+    public Apartment getApartmentById(@RequestParam("apartmentId") String apartmentId) {
+       return this.apartmentService.getApartmentById(apartmentId);
+    }
 }
--- FlatM8/src/main/java/FlatM8/service/ApartmentService.java
+++ FlatM8/src/main/java/FlatM8/service/ApartmentService.java
@@ -1,11 +1,26 @@
 package FlatM8.service;
 
+import FlatM8.model.Apartment;
 import FlatM8.repository.ApartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.UUID;
 
 public class ApartmentService {
 
+    private ApartmentRepository apartmentRepository;
     @Autowired
     public ApartmentService(ApartmentRepository apartmentRepository) {
+    }
+
+
+    public List<Apartment> getAllApartments() {
+        return this.apartmentRepository.findAll();
+    }
+
+    public Apartment getApartmentById(String apartmentId) {
+        return this.apartmentRepository.findById(UUID.fromString(apartmentId)).orElse(null);
     }
 }
